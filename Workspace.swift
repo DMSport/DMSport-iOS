2e07f3e313028a5cd3ec6933e06d6867e650ecfa
InhyeKang <kanginhae0826@gmail.com>
2022-10-05 22:44:29 +0900

    ⚙️ :: (#1) tuist project setting

---
+++ Workspace.swift
@@ -0,0 +1,11 @@
+import ProjectDescription
+
+let workspace = Workspace(
+    name: "DMSport-iOS",
+    projects: [
+        "DMSport",
+        "Service"
+    ],
+    fileHeaderTemplate: nil,
+    additionalFiles: []
+)
