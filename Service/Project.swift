80ee1003c8b0f5ec613731d7cf9dc245833025ab
InhyeKang <kanginhae0826@gmail.com>
2022-10-05 11:49:58 +0900

    🪓 :: Tuist 삭제

--- Service/Project.swift
+++
@@ -1,9 +0,0 @@
-import ProjectDescription
-import ProjectDescriptionHelpers
-
-let project = Project.makeModule (
-    name: "DMSport-iOS",
-    platform: .iOS,
-    deploymentTarget: .iOS(targetVersion: "14.0", devices: [.iphone, .ipad]),
-    dependencies: []
-)
