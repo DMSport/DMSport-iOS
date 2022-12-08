9347abcf5b3dd3b4ec7d95aa1363fa0d27d21a88
InhyeKang <kanginhae0826@gmail.com>
2022-12-08 14:29:30 +0900

    ♻️ :: code refactoring

--- DMSport/Sources/Scene/Vote/PositionVoteVC/PositionVoteVM.swift
+++ DMSport/Sources/Scene/Vote/PositionVoteVC/PositionVoteVM.swift
@@ -20,26 +20,6 @@ class PositionVoteVM {
     func transfrom(_ input: Input) -> Output {
         let voteResult = PublishRelay<Bool>()
         
-        //        input.voteID.asObservable()
-        //            .flatMap { voteID -> Single<NetworkingResult> in
-        //                self.mainProvider.rx.request(.postVoteAndrevoke(_voteID: voteID))
-        //                    .map { _ -> NetworkingResult in return .deleteOk }
-        //                    .catch { error in
-        //                        return Single.error(MyAPIError.empty)
-        //                    }
-        //            }.subscribe(onNext: { res in
-        //                switch res {
-        //                case .deleteOk:
-        //                    voteResult.accept(true)
-        //                default:
-        //                    voteResult.accept(false)
-        //                }
-        //            })
-        //            .disposed(by: disposeBag)
-        
-        
-        //        input.voteID.asObservable()
-        //            .subscribe(onNext: { id in
         self.mainProvider.rx.request(.postVoteAndrevoke(_voteID: input.voteID))
             .subscribe { res in
                 switch res {
@@ -55,26 +35,6 @@ class PositionVoteVM {
                     print(error)
                 }
             }.disposed(by: disposeBag)
-        //            }).disposed(by: disposeBag)
-        
-        //        input.buttonDidTap.asObservable()
-        //            .subscribe(onNext: {
-        //                self.mainProvider.rx.request(.postVoteAndrevoke(_voteID: <#T##Int#>))
-        //                    .subscribe { res in
-        //                        switch res {
-        //                        case .success(let result):
-        //                            debugPrint(result)
-        //                            switch result.statusCode {
-        //                            case 204:
-        //                                voteResult.accept(true)
-        //                            default:
-        //                                voteResult.accept(false)
-        //                            }
-        //                        case .failure(let error):
-        //                            print(error)
-        //                        }
-        //                    }.disposed(by: self.disposeBag)
-        //            }).disposed(by: disposeBag)
         
         return Output(voteResult: voteResult)
     }
